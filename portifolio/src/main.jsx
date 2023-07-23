7177246bda616377f5a94893bc3db506c5dab9f8
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2023-07-23 09:11:35 -0300

    ff

--- portifolio/src/main.jsx
+++ portifolio/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Home from './Home/home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App nome='willian'/>
+   
+    <Home></Home>
   </React.StrictMode>,
 )
