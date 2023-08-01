3a6b606840a1930a2441f7c073c94232dcd7fbba
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2023-07-31 22:11:22 -0300

    ass

--- portifolio/src/Skills/skills.jsx
+++ portifolio/src/Skills/skills.jsx
@@ -8,14 +8,24 @@ function Skills() {
 
   return (
     <>
-    <section>
+    <section className='skills' >
         <h1>Minhas Especialidades</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae atque reprehenderit illo repellat ad? Saepe, beatae iste illo illum animi in voluptatibus molestiae cum doloremque itaque at iure, placeat dolorum!</p>
 
         <section className='cards'>
             <div className="card">
                 <h1>React.Js</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias consequuntur distinctio perspiciatis aliquid minima cupiditate excepturi illum quaerat expedita sequi, velit, est sed quia omnis impedit architecto vero fuga laborum?</p>
+                <ul>
+                    <li>Proos</li>
+                    <li>useState</li>
+                    <li>useEffect</li>
+                    <li>useMemo</li>
+                    <li>useCallback</li>
+                    <li>LocalStorage</li>
+                    <li>Renderização Condicional</li>
+                    <li>Componentes de class</li>
+                    
+                </ul>
             </div>
             <div className="card">
                 <h1>JavsScript</h1>
