7415b726858d5ffd90c8dc2a69f8050e4514fa52
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2023-09-28 21:07:03 -0300

    atualização do meu portifolio

--- portifolio/src/Home/home.jsx
+++ portifolio/src/Home/home.jsx
@@ -11,37 +11,35 @@ function Home() {
 
   return (
     <>
-    <header>
-        <ul className='top' >
-            <li><a href="">Home</a></li>
-            <li><a href="">About-Me</a></li>
-            <li><a href="">Skills</a></li>
-            <li><a href="">Projects</a></li>
-            <button>Contact</button>
-        </ul>
-    </header>
+    <div className="home"> 
+      <header>
+          <ul className='top' >
+              <li><a href="">Home</a></li>
+              <li><a href="">About-Me</a></li>
+              <li><a href="">Skills</a></li>
+              <li><a href="">Projects</a></li>
+              <button>Contact</button>
+          </ul>
+      </header>
 
-    <main>
-      <section className='home'>
-         <section>
-           <ul className='left' >
-            <li><a href="#"><img src={Instagram} alt=""/></a></li>
-            <li><a href="#"><img src={GitHub} alt=""/></a></li>
-            <li><a href="#"><img src={Linkedin} alt=""/></a></li>
-           </ul>
+        <section className='center'>
+          <section>
+            <ul className='left' >
+              <li><a href="#"><img src={Instagram} alt=""/></a></li>
+              <li><a href="#"><img src={GitHub} alt=""/></a></li>
+              <li><a href="#"><img src={Linkedin} alt=""/></a></li>
+            </ul>
+          </section>
+
+          <section className='title' >
+              <h1>I'M Willian Reis</h1>
+            <p>Web Developer</p>
+    
         </section>
-
-         <section className='center' >
-             <h1>I'M Willian Reis</h1>
-           <p>Web Developer</p>
-  
-      </section>
-      </section>
-
-      
+        </section>
+    </div>
     
 
-    </main>
     
 
       
